64592dd94b4bfe527df7f017f78a5c5256b65fba
Kai.He <kai.he1@thoughtworks.com>
2020-08-15 16:00:54 +0800

    [kai.he] feat: implement Header component Style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import HomeIcon from './home.png'
+import PlusIcon from './plus.png'
+import ShoppingCardIcon from './shopping-card.png'
+import './Header.css'
 
 class Header extends React.Component {
     render() {
         return (
-            <div>
-                <NavLink to='/'>商城</NavLink>
-                <NavLink to='/order/'>订单</NavLink>
-                <NavLink to='/commodity/'>添加商品</NavLink>
+            <div className='header'>
+                <NavLink className='header-nav' activeStyle={{backgroundColor: '#168FFD'}} exact to='/'><img
+                    className='header-nav-icon' src={HomeIcon} alt='home'/>商城</NavLink>
+                <NavLink className='header-nav' activeStyle={{backgroundColor: '#168FFD'}} exact to='/order/'><img
+                    className='header-nav-icon' src={ShoppingCardIcon} alt='shopping-card'/>订单</NavLink>
+                <NavLink className='header-nav' activeStyle={{backgroundColor: '#168FFD'}} exact to='/commodity/'><img
+                    className='header-nav-icon' src={PlusIcon} alt='plus'/>添加商品</NavLink>
             </div>
         );
     }
