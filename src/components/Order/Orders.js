d2c3bbce7691f620d2b35ea3a4b2b89fc97f5f49
Kai.He <kai.he1@thoughtworks.com>
2020-08-15 20:33:45 +0800

    [kai.he] feat: do not display the order table when there is no order

--- src/components/Order/Orders.js
+++ src/components/Order/Orders.js
@@ -2,6 +2,7 @@ import React from "react";
 import OrderRecord from "./OrderRecord";
 import {deleteFetch, get} from "../../commons/FetcUtlils";
 import './Order.css'
+import {Link} from "react-router-dom";
 
 class Orders extends React.Component {
     constructor(props) {
@@ -31,26 +32,35 @@ class Orders extends React.Component {
         })
     }
 
+    renderOrders() {
+        if (this.state.orders.length === 0) {
+            return <h3>暂无订单，返回<Link exact to='/'>商城页面</Link>继续购买</h3>
+        } else {
+            return <table className='order-table' cellSpacing="0" cellPadding="0">
+                <thead>
+                <tr className='order-table-tr order-table-header'>
+                    <th>名字</th>
+                    <th>单价</th>
+                    <th>数量</th>
+                    <th>单位</th>
+                    <th>操作</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.orders.map((order, index) => <OrderRecord key={index}
+                                                                      order={order}
+                                                                      index={index}
+                                                                      handlerDeleteOrder={this.handlerDeleteOrder}/>)}
+                </tbody>
+            </table>
+        }
+    }
+
     render() {
         return (
             <div className='order'>
-                <table className='order-table' cellSpacing="0" cellPadding="0">
-                    <thead>
-                    <tr className='order-table-tr order-table-header'>
-                        <th>名字</th>
-                        <th>单价</th>
-                        <th>数量</th>
-                        <th>单位</th>
-                        <th>操作</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.state.orders.map((order, index) => <OrderRecord key={index}
-                                                                          order={order}
-                                                                          index={index}
-                                                                          handlerDeleteOrder={this.handlerDeleteOrder}/>)}
-                    </tbody>
-                </table>
+                {this.renderOrders()}
+
             </div>
         );
     }
